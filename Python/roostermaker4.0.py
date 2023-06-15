d35986989938825a13fb83aadf30700fef0d9ce2
MerlijnTest <merlijnfolkerts@gmail.com>
2023-06-15 13:11:37 +0200

    Hij werkt!!!!
    Soort van

--- Python/roostermaker4.0.py
+++ Python/roostermaker4.0.py
@@ -1,8 +1,11 @@
-# state = totale indeling
+import copy
+
+
 class Node():
-    def __init__(self, state):
-        self.state = state
-            
+    def __init__(self, indeling, score, sporten):
+        self.indeling = indeling
+        self.score = score
+        self.sporten = sporten
 
 class Dijkstrafrontier():
     def __init__(self):
@@ -12,16 +15,17 @@ class Dijkstrafrontier():
         self.frontier.append(node)
     
     def remove(self):
-        self.frontier.sort(key=lambda node: node.state[0])
+        self.frontier.sort(key=lambda node: node.score)
         node = self.frontier[0]
         self.frontier = self.frontier[1:]
         return node
 
             
 class Sport:
-    def __init__(self, naam, max_cap):
+    def __init__(self, naam, max_cap, leerlingen):
         self.naam = naam
         self.max_cap = max_cap
+        self.leerlingen = leerlingen
         
         
 class Leerling:
@@ -55,7 +59,7 @@ def score_voor_n_keuze(x):
     return 0.3*(x+1)**2 + 2*(x+1) - 2.3
     
 def print_resultaat(node):
-    print(node.state)
+    print(node.indeling)
     quit()
 
     
@@ -67,7 +71,7 @@ with open("/home/merlijn/Desktop/GitHub/Sportmodules/Python/keuzes.txt") as f:
     n_keuzes = int(f.readline().removeprefix("Aantal keuzes: "))
     sporten_objects = []
     for sport in sporten:
-        sport = Sport(sport, cap_req(sport, min=0))
+        sport = Sport(sport, cap_req(sport, min=0), [])
         sporten_objects.append(sport)
     sporten = sporten_objects
     for regel in f:
@@ -77,26 +81,32 @@ with open("/home/merlijn/Desktop/GitHub/Sportmodules/Python/keuzes.txt") as f:
             leerling = Leerling(leerling[0], list(leerling[1:]))
             leerlingen.append(leerling)
         
-lijst = list([0])
-start = Node(lijst)        
+
+start = Node([], 0, sporten)        
 frontier = Dijkstrafrontier()
 frontier.add(start)
 
 while True:
     parent = frontier.remove()
-    if len(parent.state) == len(leerlingen) + 1:
+    if len(parent.indeling) >= len(leerlingen):
         print_resultaat(parent)
+    
+
+    leerling = leerlingen[len(parent.indeling)]
+    n = 0
+    for sport in parent.sporten:
         
-    diepte = len(parent.state) - 1
-    leerling = leerlingen[diepte]
-    for sport in sporten:
-        if len(parent.state) - 1 >= sport.max_cap:
+        if len(sport.leerlingen) >= sport.max_cap:
             continue
         
-        child = Node(parent.state.append(sport.naam))
-        
+        child = copy.deepcopy(parent)
+        child.indeling.append(sport.naam)
+        child.sporten[n].leerlingen.append(leerling.naam)
         if sport.naam in leerling.keuzes:
-            child.state[0] += score_voor_n_keuze(leerling.keuzes.index(sport.naam))
+            child.score += score_voor_n_keuze(leerling.keuzes.index(sport.naam))
         else:
-            child.state[0] += score_voor_n_keuze(n_keuzes + 1)
+            child.score += score_voor_n_keuze(n_keuzes)
+        
+        n += 1
+        
         frontier.add(child)
